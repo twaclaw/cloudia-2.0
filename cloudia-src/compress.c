184c334f850bb8d07589ccebee8269017d380df0
Diego Sandoval <dsandoval@semtech.com>
2023-04-16 15:52:21 +0200

    Updated compress functions

--- cloudia-src/compress.c
+++ cloudia-src/compress.c
@@ -1,5 +1,7 @@
 #include "compress.h"
 
+uint8_t MASK[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+
 #define min(a,b)                                \
     ({  __typeof__ (a) _a = (a);                \
         __typeof__ (b) _b = (b);                \
@@ -10,13 +12,13 @@
         _a < 0 ? _a : -a; })
 
 
-void reset(compress_t *c)
+void compress_reset(compress_t *c)
 {
     c->byte_ptr = 0;
     c->bit_ptr = 0;
 }
 
-void add(compress_t *c, uint32_t value, uint8_t nbits)
+void compress_add(compress_t *c, uint32_t value, uint8_t nbits)
 {
     while (nbits)
     {
@@ -39,16 +41,16 @@ void add(compress_t *c, uint32_t value, uint8_t nbits)
     }
 }
 
-void add_with_sign(compress_t *c, int32_t value, uint8_t nbits)
+void compress_add_with_sign(compress_t *c, int32_t value, uint8_t nbits)
 {
     if (value < 0)
     {
-        add(c, 1, 1);
+        compress_add(c, 1, 1);
     }
     else
     {
-        add(c, 0, 1);
+        compress_add(c, 0, 1);
     }
 
-    add(c, abs(value), nbits);
+    compress_add(c, abs(value), nbits);
 }
--- cloudia-src/compress.h
+++ cloudia-src/compress.h
@@ -1,14 +1,17 @@
-#ifndef _compress_
-#define _compress_
+#ifndef __compress__
+#define __compress__
 
 #include <inttypes.h>
 
 #define COMPRESS_BUFF_SIZE 200
-uint8_t MASK[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
 
 typedef struct {
     uint8_t buff[COMPRESS_BUFF_SIZE];
     uint8_t bit_ptr;
     uint16_t byte_ptr;
 } compress_t;
-#endif //_compress_
+
+void compress_reset(compress_t *c);
+void compress_add(compress_t *c, uint32_t value, uint8_t nbits);
+void compress_add_with_sign(compress_t *c, int32_t value, uint8_t nbits);
+#endif //__compress__
